f4e477260ebfe31ee8d64995e6a4f2b14ffb5187
Archer Scherer <ascherer831@gmail.com>
2021-06-12 21:37:32 -0500

    fixed all of the problems with the provided broken project

--- poorly_coded_store/views.py
+++ poorly_coded_store/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.db.models import Sum
 from .models import Order, Product
 
 def index(request):
@@ -7,21 +8,49 @@ def index(request):
     }
     return render(request, "store/index.html", context)
 
+def process(request):
+    if request.method == 'POST':
+        order_item = Product.objects.filter(id=request.POST['id'])
+        order_quant = int(request.POST['quantity'])
+        item_price = float(order_item[0].price)
+        order_total = item_price * order_quant
+        Order.objects.create(quantity_ordered = order_quant, total_price = order_total)
+        return redirect('/checkout')
+    else:
+        return redirect('/')
+        
+
+# def checkout(request):
+#     current_order = Order.objects.last()
+#     item_quant = current_order['quantity']
+#     price_from_form = float(request.POST["price"])
+#     total_charge = quantity_from_form * price_from_form
+#     all_orders_quant = 0
+#     all_orders_price = 0
+#     orders = Order.objects.all()
+#     for order in orders:
+#         all_orders_quant += order['quantity_ordered']
+#         all_orders_price += order['total_price']
+#     context = {
+#         "all_orders": Order.objects.all(),
+#         "all_price": all_orders_price,
+#         "all_quant": all_orders_quant
+#     }
+#     print("Charging credit card...")
+#     return render(request, "store/checkout.html", context)
 def checkout(request):
-    quantity_from_form = int(request.POST["quantity"])
-    price_from_form = float(request.POST["price"])
-    total_charge = quantity_from_form * price_from_form
-    all_orders_quant = 0
-    all_orders_price = 0
-    orders = { 'all_orders':Order.objects.all()}
-    for order in Order.objects.all():
-        all_orders_quant += order['quantity_ordered']
-        all_orders_price += order['total_price']
-    context = { 
-        "all_orders": Order.objects.all(),
-        "all_price": all_orders_price,
-        "all_quant": all_orders_quant
+    total_spent = 0
+    total_items = 0
+    orders = Order.objects.all()
+
+    for order in orders:
+        total_spent += order.total_price
+        total_items += order.quantity_ordered
+    
+        context = {
+        "current_order": Order.objects.last(),
+        "total_spent": total_spent,
+        "total_items": total_items,
     }
-    print("Charging credit card...")
-    Order.objects.create(quantity_ordered=quantity_from_form, total_price=total_charge)
+
     return render(request, "store/checkout.html", context)
